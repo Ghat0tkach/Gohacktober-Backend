eb454568caf686394218862f47de85041fb29edb
ghat0tkach <awesomevikram3@gmail.com>
2025-01-03 23:14:19 +0530

    feat:fix issues
    Signed-off-by: ghat0tkach <awesomevikram3@gmail.com>

--- config/config.go
+++ config/config.go
@@ -5,11 +5,13 @@ import "os"
 type Config struct {
 	GithubToken string
 	GithubOrg   string
+	FrontendURL string
 }
 
 func Load() (*Config, error) {
 	return &Config{
 		GithubToken: os.Getenv("GITHUB_TOKEN"),
 		GithubOrg:   os.Getenv("GITHUB_ORG"),
+		FrontendURL: os.Getenv("FRONTEND_URL"),
 	}, nil
 }
